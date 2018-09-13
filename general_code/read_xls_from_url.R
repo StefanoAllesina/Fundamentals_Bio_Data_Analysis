9edd8ef4bd0dd6caedcaf0a658708be78a0bbc7e
Stefano Allesina <sallesina@uchicago.edu>
2018-09-13 12:09:36 -0500

    Finished lecture linear models

--- general_code/read_xls_from_url.R
+++ general_code/read_xls_from_url.R
@@ -14,3 +14,10 @@ read_xls_from_url <- function(my_url){
   unlink(tf) # delete temporary file
   return(df)
 }
+
+read_csv2_in_zip_file <- function(my_url, my_file_name){
+  GET(my_url, write_disk(tf <- tempfile(fileext = ".zip"))) # download into temp file
+  df <- read_csv2(unz(tf, my_file_name))
+  unlink(tf)
+  return(df)
+}
